4eda3a1dfb7a6d2c777d5884d183794640ce3511
Lewis Kirkaldie <lewis.kirkaldie@gmail.com>
2018-04-04 09:13:55 +0100

    -set srt package to previous working master release

--- get-SRT-libraries.ps1
+++ get-SRT-libraries.ps1
@@ -2,7 +2,7 @@
 #feel free to re-point this to build our tools against a different SRT library
 #will require PS5 to unzip - if it fails, you can just unzip by hand to the toolkit directory
 
-$SRT_Package_url = "https://ci.appveyor.com/api/buildjobs/5p9a6jx5xi9cf5h6/artifacts/SRT-dev-Release-1.0.456.zip"
+$SRT_Package_url = "https://ci.appveyor.com/api/buildjobs/osib2u5l3gwl9w34/artifacts/SRT-master-Release-1.0.26.zip"
 
 Write-Host "Downloading ZIP with SRT package... please be patient"
 
