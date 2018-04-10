4e8df6526b3cb18e4f50b2a00d7825230b9e7cbb
Lewis Kirkaldie <lewis.kirkaldie@gmail.com>
2018-04-09 22:56:56 -0700

    -bump SRT to v1.3.0 release library

--- get-SRT-libraries.ps1
+++ get-SRT-libraries.ps1
@@ -2,11 +2,13 @@
 #feel free to re-point this to build our tools against a different SRT library
 #will require PS5 to unzip - if it fails, you can just unzip by hand to the toolkit directory
 
-$SRT_Package_url = "https://ci.appveyor.com/api/buildjobs/yykwgiacyslle501/artifacts/SRT-master-Release-1.0.423.zip"
+$SRT_Package_url = "https://ci.appveyor.com/api/buildjobs/2cqoxhdar6ska5r8/artifacts/SRT-master-Release-1.0.469.zip"
+
+$SRTPackageName =  Split-Path -Path $url -Leaf
 
 Write-Host "Downloading ZIP with SRT package... please be patient"
 
 #$ProgressPreference = 'SilentlyContinue'
 
-iwr -ContentType "application/octet-stream" -Uri $SRT_Package_url -OutFile ./srtpackage.zip
-Expand-Archive ./srtpackage.zip ./_toolkits/srt
+iwr -ContentType "application/octet-stream" -Uri $SRT_Package_url -OutFile ./$SRTPackageName
+Expand-Archive ./$SRTPackageName ./_toolkits/srt -force
