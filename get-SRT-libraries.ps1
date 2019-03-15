8539a835652b1659f301bb2533493579adce687c
LewK2 <lewis.kirkaldie@gmail.com>
2019-03-15 12:26:12 +0000

    -move to SRT 1.3.2 -update lib / include path to refer to pthreads in dir with bitness in name -fix warning about int -> bool conversion

--- get-SRT-libraries.ps1
+++ get-SRT-libraries.ps1
@@ -2,7 +2,7 @@
 #feel free to re-point this to build our tools against a different SRT library
 #will require PS5 to unzip - if it fails, you can just unzip by hand to the toolkit directory
 
-$SRT_Package_url = "https://ci.appveyor.com/api/buildjobs/2cqoxhdar6ska5r8/artifacts/SRT-master-Release-1.0.469.zip"
+$SRT_Package_url = "https://ci.appveyor.com/api/buildjobs/7tmb6jhxtdgo84a3/artifacts/SRT-Branch_v1.3.2-Release-Winx64-2015-1.3.2.76.zip"
 
 $SRTPackageName =  Split-Path -Path $SRT_Package_url -Leaf
 
